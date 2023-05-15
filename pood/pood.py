a0c08a64ff69982e11ff89de12931b7e28d9a8f3
LilMirts <mairo.trump@gmail.com>
2023-05-15 15:11:03 +0300

    Update pood.py

--- pood/pood.py
+++ pood/pood.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+
 class Client:
     def __init__(self, name, balance, is_gold_client=False):
         self.name = name
@@ -10,9 +11,15 @@ class Client:
         self.history = {}
 
     def checkout(self):
-        if self.cart.total_cost > self.balance:
+        total_cost = self.cart.total_cost
+        if self.is_gold_client:
+            total_cost = total_cost * 0.9
+
+        if total_cost > self.balance:
             raise NotEnoughMoneyException()
 
+        for item in self.cart.items:
+            item.buy(self.cart.items[qty])
         self.history[datetime.now()] = self.cart.items  # items = {"milk": 3}
         self.cart.clear()
 
@@ -22,10 +29,43 @@ class Client:
             history_str += time.strftime("%m.%d.%Y") + "\n"
             items = self.history[time]
             for item in items:
-                history_str += f"{item} x{items[item]}\n"
+                history_str += f"  {item} x{items[item]}\n"
             history_str += "\n"
 
 
 class NotEnoughMoneyException(Exception):
     pass
 
+
+class Item:
+    def __init__(self, name, price, qty):
+        self.name = name
+        self.price = price
+        self.qty = qty
+
+    def __repr__(self):
+        return self.name
+
+    def buy(self, qty):
+        self.qty -= qty
+
+
+class ShoppingCart:
+    def __init__(self):
+        self.items = {}  # {"milk": 3}
+
+    def add_items(self, item, qty):
+        if item.qty < qty:
+            raise NotEnoughItemsException
+        else:
+            if item in self.items:
+                self.items[item] += qty
+            else:
+                self.items[item] = qty
+
+    def remove_items(self, item, qty):
+        pass
+
+
+class NotEnoughItemsException(Exception):
+    pass
