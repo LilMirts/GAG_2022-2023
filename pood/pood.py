49c9fb178ed01ee60c9a7ac6473d4c97ed8f63d7
Mirts <mairo.trump@gmail.com>
2023-05-19 12:29:58 +0300

    Update pood.py

--- pood/pood.py
+++ pood/pood.py
@@ -2,8 +2,9 @@ from datetime import datetime
 
 
 class Client:
-    def __init__(self, name, balance, is_gold_client=False):
-        self.name = name
+
+    def __init__(self, id, balance, is_gold_client=False):
+        self.id = id
         self.balance = balance
         self.is_gold_client = is_gold_client
 
@@ -11,7 +12,7 @@ class Client:
         self.history = {}
 
     def checkout(self):
-        total_cost = self.cart.total_cost
+        total_cost = self.cart.get_total_cost
         if self.is_gold_client:
             total_cost = total_cost * 0.9
 
@@ -19,8 +20,8 @@ class Client:
             raise NotEnoughMoneyException()
 
         for item in self.cart.items:
-            item.buy(self.cart.items[qty])
-        self.history[datetime.now()] = self.cart.items  # items = {"milk": 3}
+            item.buy(self.cart.items[item])
+        self.history[datetime.now()] = self.cart.items  # items = {milk: 3}
         self.cart.clear()
 
     def get_history(self):
@@ -38,25 +39,33 @@ class NotEnoughMoneyException(Exception):
 
 
 class Item:
+
     def __init__(self, name, price, qty):
         self.name = name
         self.price = price
-        self.qty = qty
+        self.stock_qty = qty
 
     def __repr__(self):
         return self.name
 
     def buy(self, qty):
-        self.qty -= qty
+        self.stock_qty -= qty
 
 
 class ShoppingCart:
+
     def __init__(self):
-        self.items = {}  # {"milk": 3}
+        self.items = {}  # {milk: 3}
+
+    def get_total_cost(self):
+        total = 0
+        for item in self.items:
+            total += self.items[item] * item.price
+        return total
 
     def add_items(self, item, qty):
-        if item.qty < qty:
-            raise NotEnoughItemsException
+        if item.stock_qty < qty:
+            raise NotEnoughItemsInStockException
         else:
             if item in self.items:
                 self.items[item] += qty
@@ -64,8 +73,44 @@ class ShoppingCart:
                 self.items[item] = qty
 
     def remove_items(self, item, qty):
-        pass
+        if item not in self.items:
+            raise RemovableItemNotInCartException()
+        if self.items[item] < qty:
+            raise NotEnoughItemsInCartException()
+        self.items[item] -= qty
+        if self.items[item] == 0:
+            del self.items[item]
+
+    def clear(self):
+        self.items = {}
 
 
-class NotEnoughItemsException(Exception):
+class NotEnoughItemsInStockException(Exception):
     pass
+
+
+class RemovableItemNotInCartException(Exception):
+    pass
+
+
+class NotEnoughItemsInCartException(Exception):
+    pass
+
+
+class Store:
+
+    def __init__(self):
+        self.stock = []
+        self.clients = []
+
+    def register_client(self, id, balance, is_gold_client=False):
+        self.clients.append(Client(id, balance, is_gold_client))
+
+    def add_to_stock(self, name, price, qty):
+        self.stock.append(Item(name, price, qty))
+
+    def get_history(self):
+        history = {}
+        for client in self.clients:
+            history.update(client.history)
+        return history
