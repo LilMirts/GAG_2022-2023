083df25b2748a0759729bb65e91eb0519103c5a7
LilMirts <mairo.trump@gmail.com>
2023-06-05 14:43:07 +0300

    Update pood.py

--- pood/pood.py
+++ pood/pood.py
@@ -2,8 +2,9 @@ from datetime import datetime
 
 
 class Client:
-
+    
     def __init__(self, id, balance, is_gold_client=False):
+        """Initialize the Client class, given the id, balance and type of the client."""
         self.id = id
         self.balance = balance
         self.is_gold_client = is_gold_client
@@ -12,6 +13,7 @@ class Client:
         self.history = {}
 
     def checkout(self):
+        """Make a purchase, removing the total cost of the cart from client's balance and clearing the cart."""
         total_cost = self.cart.get_total_cost()
         if self.is_gold_client:
             total_cost = round(total_cost * 0.9, 2)
@@ -26,6 +28,7 @@ class Client:
         self.cart.clear()
 
     def get_history(self):
+        """Return the history of the client's purchases as a string."""
         history_str = ""
         for time in sorted(self.history.keys(), reverse=True):
             history_str += time.strftime("%m.%d.%Y") + "\n"
@@ -43,14 +46,17 @@ class NotEnoughMoneyException(Exception):
 class Item:
 
     def __init__(self, name, price, qty):
+        """Initialize the Item class, given the name, price and stock quantity of the item."""
         self.name = name
         self.price = price
         self.stock_qty = qty
 
     def __repr__(self):
+        """Represent the item as a string."""
         return self.name
 
     def buy(self, qty):
+        """Remove given quantity of the item from stock, if possible."""
         if qty > self.stock_qty:
             raise NotEnoughItemsInStockException
         self.stock_qty -= qty
@@ -59,15 +65,18 @@ class Item:
 class ShoppingCart:
 
     def __init__(self):
+        """Initialize the ShoppingCart class without any items in it."""
         self.items = {}  # {milk: 3}
 
     def get_total_cost(self):
+        """Return the total cost of items in the shopping cart."""
         total = 0
         for item in self.items:
             total += self.items[item] * item.price
         return total
 
     def add_items(self, item, qty):
+        """Add the given quantity of given item to the cart."""
         if item in self.items:
             if item.stock_qty - self.items[item] < qty:
                 raise NotEnoughItemsInStockException
@@ -80,6 +89,7 @@ class ShoppingCart:
                 self.items[item] = qty
 
     def remove_items(self, item, qty):
+        """Remove the given quantity of given item from the cart."""
         if item not in self.items:
             raise RemovableItemNotInCartException()
         if self.items[item] < qty:
@@ -89,6 +99,7 @@ class ShoppingCart:
             del self.items[item]
 
     def clear(self):
+        """Empty the cart."""
         self.items = {}
 
 
@@ -107,16 +118,19 @@ class NotEnoughItemsInCartException(Exception):
 class Store:
 
     def __init__(self):
+        """Initialize the Store class."""
         self.stock = []
         self.clients = []
 
     def register_client(self, id, balance, is_gold_client=False):
+        """Add a client to the store."""
         for client in self.clients:
             if client.id == id:
                 raise ClientIdAlreadyTakenException()
         self.clients.append(Client(id, balance, is_gold_client))
 
     def add_to_stock(self, name, price, qty):
+        """Add an item to stock or increase the quantity of an item in stock."""
         for item in self.stock:
             if item.name == name:
                 if item.price == price:
@@ -127,6 +141,7 @@ class Store:
         self.stock.append(Item(name, price, qty))
 
     def get_history(self):
+        """Return the history of purchases in the store as a dictionary."""
         history = {}
         for client in self.clients:
             history.update(client.history)
